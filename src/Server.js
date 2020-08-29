afef6804f61781798b7c269d7ff174abccd0bea5
Hri7566 <hri7566@gmail.com>
2020-08-29 12:10:15 -0400

    Start work on websocket server

--- src/Server.js
+++ src/Server.js
@@ -1,11 +1,15 @@
 // Rank server module
+const WebSocket = require('ws');
 
 module.exports = class Server {
     constructor (port) {
         this.ranks = require('./database/ranks.json');
+        this.wss = new WebSocket.Server({
+            port: port
+        });
     }
 
     start() {
-
+        
     }
 }
