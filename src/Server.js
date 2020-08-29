e6123adf1703801a84889e02dbcdb72d8cc663f1
Hri7566 <hri7566@gmail.com>
2020-08-29 16:16:54 -0400

    i need help with websockets

--- src/Server.js
+++ src/Server.js
@@ -10,6 +10,21 @@ module.exports = class Server {
     }
 
     start() {
-        
+        // someone help me - Hri7566
+        this.wss.on('request', (req) => {
+            const conn = req.accept(null, req.origin);
+
+            conn.on('connect')
+
+            conn.on('message', (msg) => {
+                console.log(`Message: ${msg.utf8Data}`);
+                conn.sendUTF(`hi :P`);
+            });
+            
+            conn.on('close', (code, desc) => {
+                console.log("Client left");
+            });
+        });
+        console.log("WS server started");
     }
 }
